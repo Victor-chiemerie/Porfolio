e6d5c5f03cc7bc83c65c6e37e6aca1a339e671a7
Victor-chiemerie <victorchiemerie02@gmail.com>
2022-12-21 23:20:28 +0100

    Set work-pages to load dynaically from js file

--- main.js
+++ main.js
@@ -13,3 +13,137 @@ closeMenu.addEventListener('click', toggleMenu);
 menuLinks.forEach((link) => {
   link.addEventListener('click', toggleMenu);
 });
+
+// Modal section
+
+const workopen = document.querySelectorAll('#project-button');
+const modalPage = document.querySelector('#modal-section');
+const closeProjects = document.querySelector('#close-projects');
+const workPages = document.querySelector('#work-pages');
+const mainProject = {
+  name: 'Multi-Post Stories',
+  description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
+  featuredImage: 'profession1 background.png',
+  technologies: ['css', 'html', 'Bootstrap', 'Ruby'],
+  liveVersion: 'https://victor-chiemerie.github.io/',
+  source: 'https://github.com/Victor-chiemerie/Victor-chiemerie.github.io',
+}
+
+function toggleModal() {
+  modalPage.classList.toggle('hide');
+}
+
+workopen.forEach((button) => {
+  button.addEventListener('click', toggleModal);
+});
+
+closeProjects.addEventListener('click', () => modalPage.classList.add('hide'));
+
+
+const projects = [
+  {
+    id: 1,
+    name: 'Profesional Art Printing Data More',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard',
+    featuredImage: 'profession1 background.png',
+    technologies: ['html', 'Bootstrap', 'Ruby'],
+    liveVersion: 'https://victor-chiemerie.github.io/',
+    source: 'https://github.com/Victor-chiemerie/Victor-chiemerie.github.io',
+  },
+  {
+    id: 2,
+    name: 'Data Dashboard Healthcare',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard',
+    featuredImage: 'profession2 background.png',
+    technologies: ['html', 'Bootstrap', 'Ruby'],
+    liveVersion: 'https://victor-chiemerie.github.io/',
+    source: 'https://github.com/Victor-chiemerie/Victor-chiemerie.github.io',
+  },
+  {
+    id: 3,
+    name: 'Website Protfolio',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard',
+    featuredImage: 'profession3 background.png',
+    technologies: ['html', 'Bootstrap', 'Ruby'],
+    liveVersion: 'https://victor-chiemerie.github.io/',
+    source: 'https://github.com/Victor-chiemerie/Victor-chiemerie.github.io',
+  },
+  {
+    id: 4,
+    name: 'Profesional Art Printing Data More',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard',
+    featuredImage: 'profession1 background.png',
+    technologies: ['html', 'Bootstrap', 'Ruby'],
+    liveVersion: 'https://victor-chiemerie.github.io/',
+    source: 'https://github.com/Victor-chiemerie/Victor-chiemerie.github.io',
+  },
+  {
+    id: 5,
+    name: 'Data Dashboard Healthcare',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard',
+    featuredImage: 'profession2 background.png',
+    technologies: ['html', 'Bootstrap', 'Ruby'],
+    liveVersion: 'https://victor-chiemerie.github.io/',
+    source: 'https://github.com/Victor-chiemerie/Victor-chiemerie.github.io',
+  },
+  {
+    id: 6,
+    name: 'Website Protfolio',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard',
+    featuredImage: 'profession3 background.png',
+    technologies: ['html', 'Bootstrap', 'Ruby'],
+    liveVersion: 'https://victor-chiemerie.github.io/',
+    source: 'https://github.com/Victor-chiemerie/Victor-chiemerie.github.io',
+  }
+];
+
+const mainWork = document.createElement('main');
+mainWork.classList.add('first-page');
+mainWork.innerHTML = `
+        <div class="work-container">
+          <div class="work-item1">
+            <h2 class="title"> My Recent Works </h2>
+            <img class="line" src="images/line.png" alt="Middle-section">
+          </div>
+            <div class="work-item2">
+              <div class="picture"></div>
+              <div class="multi-stories">
+                  <h3 class="end-note">${mainProject.name}</h3>
+                    <p class="paragragh">
+                      ${mainProject.description}
+                    </p>
+                    <ul class="list-tag">
+                      <li class="tags">css</li>
+                      <li class="tags">html</li>
+                      <li class="tags">bootstrap</li>
+                      <li class="tags">ruby</li>
+                    </ul>
+                    <button id="project-button-main" class="buttons">See Project</button>
+              </div>
+            </div>
+        </div>
+        `
+
+        workPages.appendChild(mainWork);
+
+projects.forEach((project) => {
+  const list = document.createElement('li');
+  list.classList.add(`profession${project.id}`);
+  list.innerHTML = `
+  <div class="workings1">
+            <h2 class="topic">${project.name}</h2>
+              <p>
+                ${project.description}
+              </p>
+                <ul class="list-tag">
+                  <li class="tags">html</li>
+                  <li class="tags">bootstrap</li>
+                  <li class="tags">ruby</li>
+                </ul>
+            <button id="project-button.${project.id}" class="buttons">See Project</button>
+          </div>`
+
+          workPages.appendChild(list);
+});
+// projects[0]['id'] = 7;
+// console.log(projects[0]['id']);

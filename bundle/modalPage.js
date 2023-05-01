f64b1479704ed26c9d5c130ac335c3cbb6aeb29e
Victor-chiemerie <victorchiemerie02@gmail.com>
2023-05-01 18:18:41 +0100

    Start modal section construction

---
+++ bundle/modalPage.js
@@ -0,0 +1,14 @@
+// import
+
+// const mainWorkButton = document.getElementById('project-button-main');
+
+// const workopen = document.querySelectorAll('#project-button');
+// const modalPage = document.querySelector('#modal-section');
+// const workPages = document.querySelector('#work-pages');
+// const modalContent = document.querySelector('#modal-contents');
+
+// const toggleModal = () => {
+//   modalPage.classList.toggle('hide');
+// };
+
+// export default toggleModal;
--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const mainProject = {
   source: 'https://github.com/Victor-chiemerie/Victor-chiemerie.github.io',
 };
 
-const workPages = document.querySelector('#work-pages');
+const workPages = document.getElementById('work-pages');
 
 const mainWork = document.createElement('main');
 mainWork.classList.add('first-page');
@@ -28,7 +28,7 @@ mainWork.innerHTML = `
               </p>
               <ul class="list-tag">
               </ul>
-              <button id="project-button-main" class="buttons">See Project</button>
+              <button id="See-Main-Project" class="buttons">See Project</button>
         </div>
       </div>
 `;
@@ -46,3 +46,46 @@ tools.innerHTML = list;
 
 works();
 toggleMenu();
+
+const workopen = document.querySelectorAll('#project-button');
+const modalPage = document.getElementById('modal-section');
+const modalContent = document.getElementById('modal-contents');
+
+const toggleModal = () => {
+  modalPage.classList.toggle('hide');
+};
+
+workopen.forEach((button) => {
+  button.addEventListener('click', toggleModal);
+});
+
+const mainWorkButton = document.getElementById('See-Main-Project');
+mainWorkButton.addEventListener('click', () => {
+  modalContent.innerHTML = `<div class="title-sect">
+    <h2>${mainProject.name}</h2>
+    <h3>${mainProject.name}</h3>
+    <img src="images/close-icon.png" id="close-projects">
+  </div>
+  <ul class="modal-list-tag">
+    <li class="modal-tags">html</li>
+    <li class="modal-tags">Bootstrap</li>
+    <li class="modal-tags">Ruby on rails</li>
+  </ul>
+  <div class="second-sect">
+    <div class="second-sect-img">
+      <img class="image" src="images/${mainProject.featuredImage}" alt="Snapshoot image">
+    </div>
+    <div class="last-sect">
+      <p>${mainProject.description}</p>
+      <div class="last-buttons">
+        <a href="https://victor-chiemerie.github.io/"><button class="buttons">See Live <img src="images/Live link.png" alt="live_link"></button></a>
+        <a href="https://github.com/Victor-chiemerie/Victor-chiemerie.github.io"><button class="buttons">See Source <img src="images/github.png" alt="github"></button></a>
+      </div>
+    </div>
+  </div>`;
+
+  const closeMainProjects = document.querySelector('#close-projects');
+  closeMainProjects.addEventListener('click', () => modalPage.classList.add('hide'));
+
+  modalPage.classList.remove('hide');
+});
